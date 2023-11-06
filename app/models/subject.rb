ed6cbe70963c9719ba5c231df7d049ffa262ad8c
John Lenard Serrano Agbalo <johnlenardagbalo@gmail.com>
2023-11-06 23:21:06 +0800

    Fixing Error that subject cannot load key error 32
    8. Controllers and CRUD na done na sa iba

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,6 +1,11 @@
 class Subject < ApplicationRecord
-  scope :visible, -> { where(visible: true) }
-  scope :invisible, -> { where(visible: false) }
-  scope :sorted, -> { order(created_at: :desc) }
-  scope :search, ->(query) { where("name LIKE ?", "%#{query}%") }
+
+  has_many :pages
+
+  scope :visible, lambda { where(:visible => true) }
+  scope :invisible, lambda { where(:visible => false) }
+  scope :sorted, lambda { order("position ASC") }
+  scope :newest_first, lambda { order("created_at DESC") }
+  scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"]) }
+
 end
