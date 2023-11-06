74a75e65e1a943e4f39721729de6ccabeb69d4c7
John Lenard Serrano Agbalo <johnlenardagbalo@gmail.com>
2023-11-06 10:22:37 +0800

    5 Models and ActiveRecord
    Models and Active Record next go to the 6. Associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,6 @@
 class Subject < ApplicationRecord
+  scope :visible, -> { where(visible: true) }
+  scope :invisible, -> { where(visible: false) }
+  scope :sorted, -> { order(created_at: :desc) }
+  scope :search, ->(query) { where("name LIKE ?", "%#{query}%") }
 end
