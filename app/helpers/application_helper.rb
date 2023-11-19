592ab64f4418b339b5703b0e7e7e742e61285c6c
John Lenard Serrano Agbalo <johnlenardagbalo@gmail.com>
2023-11-19 23:51:27 +0800

    error handling
    continue study

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,7 @@
 module ApplicationHelper
-
+    def error_messages_for(object)
+        render(:partial => 'application/error_messages', :locals => { :object => object})
+    end
     def status_tag(boolean, option={})
     option[:true_text] ||= ''
     option[:false_text] ||= ''
